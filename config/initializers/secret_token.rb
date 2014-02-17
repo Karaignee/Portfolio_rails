a89c79a6b495cfcc427411541069fa9fd00744ae
NCI <nci@nci-VirtualBox.(none)>
2014-02-17 14:27:39 +0000

    git tracking

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = '3c9facec5de0aa11f481f8253c48049bdf638b61c6330813aa26bc44d56f8f9c86ae9682681ca246a47395aa609c945389e2138f8179df9d3963f4ac8e391562'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Portfolio::Application.routes.draw do
+
+  root :to => 'home#index'
+
+end
