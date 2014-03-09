f7aa55f00a6d305949af5443585d28a34a904a17
Karaignee <karen_rooney@hotmail.com>
2014-03-09 18:10:16 +0000

    precompile line moved

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-config.assets.initialize_on_precompile = false
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
@@ -9,8 +8,11 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
+
 module Portfolio
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
